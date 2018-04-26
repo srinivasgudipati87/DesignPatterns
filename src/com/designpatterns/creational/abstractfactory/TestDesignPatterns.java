4301f8fcf18ae6b9979e5e812b28bd056071ceb1
Srinivas Gudipati <srinivasgudipati321@gmail.com>
2018-04-26 08:00:10 -0400

    DS

--- src/com/designpatterns/creational/abstractfactory/TestDesignPatterns.java
+++ src/com/designpatterns/creational/abstractfactory/TestDesignPatterns.java
@@ -1,10 +1,16 @@
 package com.designpatterns.creational.abstractfactory;
 
-public class TestFactory {
+public class TestDesignPatterns {
 	public static void main(String[] args) {
-		Computer pc = ComputerFactory.getComputer("pc", "2 GB", "500GB", "2.4 GHz");
-		Computer server = ComputerFactory.getComputer("server", "16GB", "1 TB", "2.9 GHz");
-		System.out.println("Factory PC Config::" + pc);
-		System.out.println("Factory Server Config::" + server);
+		testAbstractFactory();
+	}
+
+	private static void testAbstractFactory() {
+		Computer pc = ComputerFactory
+				.getComputer(new PCFactory("2 GB", "500 GB", "2.4 GHz"));
+		Computer server = ComputerFactory
+				.getComputer(new ServerFactory("16 GB", "1 TB", "2.9 GHz"));
+		System.out.println("AbstractFactory PC Config::" + pc);
+		System.out.println("AbstractFactory Server Config::" + server);
 	}
 }
